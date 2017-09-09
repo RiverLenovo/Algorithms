772edf73c01c9a16a20f05bfb12dd39f737e0e85
RiverWalter <hcduan@vip.126.com>
2017-09-09 19:16:40 +0800

    新增一条printf语句

--- Algorithms/src/main.cpp
+++ Algorithms/src/main.cpp
@@ -2,7 +2,8 @@
 int main()
 {
     EuclidGCDTest(252, 105);
-    //printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello great programmers!\n");
     getchar();
     return 0;
 }
